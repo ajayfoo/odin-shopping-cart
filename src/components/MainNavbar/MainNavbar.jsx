d306f532689cb0754973b76b3106d129d09df52d
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-07-07 21:07:20 +0530

    feat: highlight selected page on MainNavbar

--- src/components/MainNavbar/MainNavbar.jsx
+++ src/components/MainNavbar/MainNavbar.jsx
@@ -1,15 +1,39 @@
 import { Link } from "react-router-dom";
 import classes from "./MainNavbar.module.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export default function MainNavbar({ cartCount }) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const displayCartCount = cartCount > 9 ? "+9" : cartCount;
+  const handleClick = ({ target }) => {
+    setSelectedIndex(parseInt(target.dataset.index));
+  };
   return (
     <nav className={classes["main-nav"]}>
-      <Link to="/">Home</Link>
-      <Link aria-label="cart" className={classes.cart} to="/cart">
+      <Link
+        data-index={0}
+        onClick={handleClick}
+        className={selectedIndex === 0 ? classes.selected : ""}
+        to="/"
+      >
+        Home
+      </Link>
+      <Link
+        data-index={1}
+        onClick={handleClick}
+        className={
+          selectedIndex === 1 ? classes["selected-cart"] : classes.cart
+        }
+        aria-label="cart"
+        to="/cart"
+      >
         Cart
-        <span role="alert" aria-label="shopping cart items count">
+        <span
+          data-index={1}
+          role="alert"
+          aria-label="shopping cart items count"
+        >
           {displayCartCount}
         </span>
       </Link>

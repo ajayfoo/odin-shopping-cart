9f4851f823a5c9d40c4fbd659131f52f35ae19af
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-07-07 15:06:46 +0530

    feat: add products prop to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,23 @@
 import ProductCard from "../ProductCard/ProductCard";
 import classes from "./Home.module.css";
-
-export default function Home() {
+import PropTypes from "prop-types";
+export default function Home({ products, onAdd }) {
   return (
     <div aria-label="home page" className={classes.home}>
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
-      <ProductCard
-        imgSrc="src/images/t-shirt.jpg"
-        name="White T-Shirt"
-        price={540}
-      />
+      {products.map((p) => (
+        <ProductCard
+          imgSrc={p.imgSrc}
+          name={p.name}
+          price={p.price}
+          onAdd={onAdd}
+          key={p.id}
+        />
+      ))}
     </div>
   );
 }
+
+Home.propTypes = {
+  products: PropTypes.array.isRequired,
+  onAdd: PropTypes.func.isRequired,
+};
--- src/components/Home/Home.spec.jsx
+++ src/components/Home/Home.spec.jsx
@@ -1,10 +1,49 @@
-import { describe, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { getByRole, render, screen } from "@testing-library/react";
 import Home from "./Home";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("../ProductCard/ProductCard");
 
 describe("Home", () => {
-  it("renders it", () => {
-    render(<Home products={[]} />);
+  it("renders 1 product card", () => {
+    const onAdd = vi.fn();
+    const singleProduct = {
+      name: "White T-Shirt",
+      price: 200,
+      imgSrc: "src/images/t-shirt.png",
+      id: 123,
+    };
+    const products = [singleProduct];
+
+    render(<Home products={products} onAdd={onAdd} />);
+
     screen.getByRole("generic", { name: "home page" });
+
+    const card = screen.getByRole("article", {
+      name: singleProduct.name + " product card",
+    });
+
+    expect(card).toBeVisible();
+  });
+
+  it("calls onAdd when Add To Cart is clicked", async () => {
+    const user = userEvent.setup();
+    const onAdd = vi.fn();
+    const singleProduct = {
+      name: "White T-Shirt",
+      price: 200,
+      imgSrc: "src/images/t-shirt.png",
+      id: 123,
+    };
+    const products = [singleProduct];
+    render(<Home products={products} onAdd={onAdd} />);
+    screen.getByRole("generic", { name: "home page" });
+    const card = screen.getByRole("article", {
+      name: singleProduct.name + " product card",
+    });
+    const addToCartBtn = getByRole(card, "button", { name: "Add To Cart" });
+    await user.click(addToCartBtn);
+    expect(onAdd).toBeCalled();
   });
 });

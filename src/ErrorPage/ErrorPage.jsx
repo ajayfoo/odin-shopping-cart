926ff43da834aea2789e4037942c84e50b6de125
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-07-09 20:43:55 +0530

    feat: add go to home link to ErrorPage

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,12 @@
 import { useRouteError } from "react-router-dom";
 import classes from "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
-  return <p className={classes.error}>{error.statusText || error.message}</p>;
+  return (
+    <div className={classes.error}>
+      <p>Page {error.statusText || error.message}</p>
+      <Link to="/">Go To Home</Link>
+    </div>
+  );
 }
